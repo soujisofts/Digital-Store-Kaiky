cc0ede41a5d20acc20bbe3feb888b36d20579902
soujisofts <kaikyxavier59@gmail.com>
2024-07-17 00:47:32 -0300

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ const Header = () => {
         <>
         <div>
             <div>
-                
-                <h1>Digital Store</h1>
-                <input type="text" placeholder="Digite aqui..." />
+
+                <img class="logo" src="https://media.licdn.com/dms/image/C4E0BAQGOBx_Tgf4xFA/company-logo_200_200/0/1656456553386/digitalcollegebr_logo?e=1729123200&v=beta&t=EP6-vkiEaIXSiL3cSbfsVWsfPwSDgUp7JnVvtk-nuks" alt="Digital College Logo" />
+
+                <h1 class="title-header" >Digital Store</h1>
+                <input type="text" placeholder="Pesquise seu produto..." />
                 <button>Enviar</button> 
 
             </div>
             <div>
                 <nav>
-                    <ul>
+                    <ul class="bar">
                         <li>Home</li>
                         <li>Produtos</li>
                         <li>Categorias</li>
