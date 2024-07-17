aa90275844dbaada90f72043d4ac021c5b044d2b
soujisofts <kaikyxavier59@gmail.com>
2024-07-16 23:50:54 -0300

    first main-kaiky

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+const Header = () => {
+    return (
+        <>
+        <div>
+            <div>
+                
+                <h1>Digital Store</h1>
+                <input type="text" placeholder="Digite aqui..." />
+                <button>Enviar</button> 
+
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Produtos</li>
+                        <li>Categorias</li>
+                        <li>Meus Pedidos</li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </div>
+        </>
+    );
+}
+ 
+export default Header;
